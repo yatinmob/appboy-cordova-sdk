048221f4052e7806ba215cc4c38299bfaaa85191
Julian Contreras <julian@braze.com>
2020-10-30 13:13:22 -0400

    Release Cordova SDK version 2.19.0

--- src/android/AppboyPlugin.java
+++ src/android/AppboyPlugin.java
@@ -2,6 +2,8 @@ package com.appboy.cordova;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.util.Log;
 
 import com.appboy.Appboy;
@@ -18,6 +20,7 @@ import com.appboy.events.IEventSubscriber;
 import com.appboy.models.cards.Card;
 import com.appboy.models.outgoing.AppboyProperties;
 import com.appboy.models.outgoing.AttributionData;
+import com.appboy.services.AppboyLocationService;
 import com.appboy.support.AppboyLogger;
 import com.appboy.ui.activities.AppboyContentCardsActivity;
 import com.appboy.ui.activities.AppboyFeedActivity;
@@ -39,6 +42,9 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class AppboyPlugin extends CordovaPlugin {
   private static final String TAG = String.format("Appboy.%s", AppboyPlugin.class.getName());
+
+  // Runtime permissions
+  private static final int LOCATION_REQUEST_CODE = 2;
 
   // Preference keys found in the config.xml
   private static final String APPBOY_API_KEY_PREFERENCE = "com.appboy.api_key";
@@ -84,7 +90,25 @@ public class AppboyPlugin extends CordovaPlugin {
     // Since we've likely passed the first Application.onCreate() (due to the plugin lifecycle), lets call the
     // in-app message manager and session handling now
     AppboyInAppMessageManager.getInstance().registerInAppMessageManager(this.cordova.getActivity());
+
+    initializeGeofences();
     mPluginInitializationFinished = true;
+  }
+
+  @Override
+  public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) {
+    switch (requestCode) {
+      case LOCATION_REQUEST_CODE:
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+          Log.i(TAG, "Fine location permission granted.");
+          AppboyLocationService.requestInitialization(mApplicationContext);
+        } else {
+          Log.i(TAG, "Fine location permission NOT granted.");
+        }
+        break;
+      default:
+        break;
+    }
   }
 
   @Override
@@ -540,6 +564,28 @@ public class AppboyPlugin extends CordovaPlugin {
     // Return success to the callback
     callbackContext.success();
     return true;
+  }
+
+  private void initializeGeofences() {
+    String fineLocationPermission = "android.permission.ACCESS_FINE_LOCATION";
+    if (Build.VERSION.SDK_INT >= 29) {
+      String accessBackgroundPermission = "android.permission.ACCESS_FINE_LOCATION";
+      // Get location permissions, if we need them
+      if (cordova.hasPermission(fineLocationPermission) && cordova.hasPermission(accessBackgroundPermission)) {
+        AppboyLocationService.requestInitialization(mApplicationContext);
+      } else {
+        // Request the permission
+        cordova.requestPermissions(this, LOCATION_REQUEST_CODE, new String[]{fineLocationPermission, accessBackgroundPermission});
+      }
+    } else {
+      // Get location permissions, if we need them
+      if (cordova.hasPermission(fineLocationPermission)) {
+        AppboyLocationService.requestInitialization(mApplicationContext);
+      } else {
+        // Request the permission
+        cordova.requestPermission(this, LOCATION_REQUEST_CODE, fineLocationPermission);
+      }
+    }
   }
 
   private static EnumSet<CardCategory> getCategoriesFromJSONArray(JSONArray jsonArray) throws JSONException {
